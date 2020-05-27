802abb08d1a3cd25ac9fadaf86e7220a4cc7d48a
SAFNAS <iamsafnas@gmail.com>
2020-05-27 18:55:33 +0530

    php embedded in html

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo 'hello, world';
+// echo 'hello, world';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP file</title>
+</head>
+<body>
+    <h1><?php echo 'hello world'; ?></h1>
+</body>
+</html>
