b059d1fcd6cfe42996c492df6546caab196c1352
SAFNAS <iamsafnas@gmail.com>
2020-05-27 21:35:42 +0530

    multi dim arrays part2 n pop

--- index.php
+++ index.php
@@ -2,14 +2,28 @@
 
 //multi dimensional arrays
 
-$blogs = [
-    ['mario party','mario','lorem',30],
-    ['mario kart cheats','toad','lorem',25]
-];
+// $blogs = [
+//     ['mario party','mario','lorem',30],
+//     ['mario kart cheats','toad','lorem',25]
+// ];
+
 
 //print_r($blogs)
 //print_r($blogs[1])
-print_r($blogs[1][1]) //value
+// print_r($blogs[1][1]) //value
+
+$blogs = [
+    ['title' => 'mario party','author'=>'mario','content'=>'lorem','likes'=> 30],
+    ['title' => 'mario kart cheats','author'=>'toad','content'=>'lorem','likes'=> 25]
+];
+
+//echo $blogs[1]['author']
+
+$popped = array_pop($blogs);
+print_r($popped)
+
+
+
 
 
 ?>
