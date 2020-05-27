6153662ce6fa6285037f9c887ab7f8bec8ccee32
SAFNAS <iamsafnas@gmail.com>
2020-05-27 20:23:53 +0530

    count

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
 
 $peopleOne = ['safnas','sut','vava'];
-echo $peopleOne[1];
+// echo $peopleOne[1];
 
 $peopleTwo = array('safnas','sut','vava');
-echo $peopleTwo[1];
+// echo $peopleTwo[1];
 
 $ages = [20,30,40,50];
 //echo $ages; //error
-print_r($ages);
+// print_r($ages);
 
 $ages[] = 60;
 
-print_r($ages)
+// print_r($ages);
+
+array_push($ages,70);
+// print_r($ages)
+
+echo count($ages)
 
 ?>
 
