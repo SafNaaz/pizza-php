10a6c2a6b3c9c97d8b2a812219440018470ca29a
SAFNAS <iamsafnas@gmail.com>
2020-05-27 20:22:05 +0530

    adding value to array

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
 $peopleOne = ['safnas','sut','vava'];
-echo $peopleOne[1]
+echo $peopleOne[1];
+
+$peopleTwo = array('safnas','sut','vava');
+echo $peopleTwo[1];
+
+$ages = [20,30,40,50];
+//echo $ages; //error
+print_r($ages);
+
+$ages[] = 60;
+
+print_r($ages)
 
 ?>
 
