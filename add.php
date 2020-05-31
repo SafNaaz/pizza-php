69b9365ca1a2eff64143d0831c6ce535b7f92863
SAFNAS <iamsafnas@gmail.com>
2020-05-31 11:20:10 +0530

    xss , GET and POST

---
+++ add.php
@@ -0,0 +1,40 @@
+<?php
+
+    // if(isset($_GET['submit'])){
+    //     echo $_GET['email'];
+    //     echo $_GET['title'];
+    //     echo $_GET['ingredients'];
+    // }
+
+    if(isset($_POST['submit'])){
+        echo htmlspecialchars($_POST['email']);  //prevent XSS attacks
+        echo htmlspecialchars($_POST['title']);
+        echo htmlspecialchars($_POST['ingredients']);
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include ('templates/header.php') ?>
+
+<section class="container grey-text">
+    <h4 class="center">Add a Pizza</h4>
+    <form class="white" action="add.php" method="POST">
+        <label>Your Email</label>
+        <input type="text" name="email">
+        <label>Pizza Title</label>
+        <input type="text" name="title">
+        <label>Ingredients (comma seperated)</label>
+        <input type="text" name="ingredients">
+        <div class="center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </div>
+    </form>
+</section>
+
+<?php include ('templates/footer.php') ?>
+
+
+</html>
