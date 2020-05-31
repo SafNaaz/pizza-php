ba18619536e501e4147a8ce2134d4006388c0870
SAFNAS <iamsafnas@gmail.com>
2020-05-31 17:23:20 +0530

    session and null coalesce

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,15 @@
+<?php
+
+    session_start();
+
+    if($_SERVER['QUERY_STRING'] == 'noname'){
+        unset($_SESSION['name']);
+    }
+
+    $name = $_SESSION['name'] ?? 'Guest';
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,7 +41,8 @@
         <div class="container">
             <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
-            <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
-        </ul>
+                <li class="grey-text">Hello <?php echo htmlspecialchars($name) ?></li>
+                <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
         </div>
     </nav>
