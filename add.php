192197a544918f49f3f842326318b4d4acb52c9b
SAFNAS <iamsafnas@gmail.com>
2020-05-31 11:24:24 +0530

    basic validation

--- add.php
+++ add.php
@@ -7,9 +7,33 @@
     // }
 
     if(isset($_POST['submit'])){
-        echo htmlspecialchars($_POST['email']);  //prevent XSS attacks
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']);
+        // echo htmlspecialchars($_POST['email']);  //prevent XSS attacks
+        // echo htmlspecialchars($_POST['title']);
+        // echo htmlspecialchars($_POST['ingredients']);
+
+        //check email
+        if(empty($_POST['email'])){
+            echo 'An email is required <br/>';
+        } else{
+            echo htmlspecialchars($_POST['email']);
+        }
+
+        //check title
+        if(empty($_POST['title'])){
+            echo 'A title is required <br/>';
+        } else{
+            echo htmlspecialchars($_POST['title']);
+        }
+
+        //check ingredients
+        if(empty($_POST['ingredients'])){
+            echo 'Atleast one ingredient is required <br/>';
+        } else{
+            echo htmlspecialchars($_POST['ingredients']);
+        }
+
+        
+
     }
 
 
