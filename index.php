62b9897394e8a2f040e10cedb0270f12763838fd
SAFNAS <iamsafnas@gmail.com>
2020-05-31 12:19:34 +0530

    mysql connection check

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
+    // MYSQLi and PDO (php data objects)
+    //connect to database
+    $conn = mysqli_connect('localhost','safnas','Naaz@6581', 'ninja_pizza');
 
+    if(!$conn){
+        echo 'Connection error: '.mysqli_connect_error();
+    }
 
 
 ?>
